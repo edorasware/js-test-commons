8c3ff9e2999fdc9d7071cabcb0a4135dfee28344
Alberto Mijares <alberto.mijares@edorasware.com>
2015-09-23 23:20:33 +0200

    (fix): fix toThrowContainingMatcher() bugs

--- src/matchers/to-throw-containing-matcher.js
+++ src/matchers/to-throw-containing-matcher.js
@@ -1,34 +1,20 @@
 var _ = require('lodash');
-var exceptionMeetsExpectation = false,
-    exception,
-    exceptionMessage;
 
-module.exports = function (util, customEqualityTesters) {
+module.exports = function () {
     return {
-        compare: function(actual, expected) {
-            
-            var message, passed;
+        compare: function (actual, expected) {
+            var passed = false, message, exception, exceptionMessage, expectedMessages = getExpectedMessages(expected);
 
-            if (expected === undefined) {
-                throwInvalidExpectationError();
-            } else {
-                expected = _.isArray(expected)? expected : jasmine.util.argsToArray(expected);    
-                if (expected.length === 0) {
-                    throwInvalidExpectationError();
-                }
-            }
-
-            if (!_.isFunction(actual)) { 
+            if (!_.isFunction(actual)) {
                 throw new Error('The value is not a function in matcher "toThrowContaining".');
             }
-            
+
             exception = executeAndReturnException(actual);
             exceptionMessage = exception && (exception.message || exception);
 
             if (exceptionMessage) {
-                exceptionMeetsExpectation = exceptionContainsMessages(exceptionMessage, expected);
-                message = getExceptionMessage(exception, exceptionMeetsExpectation, expected, exceptionMessage);
-                passed = exceptionContainsMessages(exceptionMessage, expected);
+                passed = exceptionContainsMessages(exceptionMessage, expectedMessages);
+                message = getExceptionMessage(exception, passed, expectedMessages, exceptionMessage);
             }
 
             return {
@@ -36,36 +22,49 @@ module.exports = function (util, customEqualityTesters) {
                 message: message
             };
         }
+    };
+
+    function getExceptionMessage(exception, passed, expectedMessages, exceptionMessage) {
+        var not = jasmine.isNot ? 'not ' : '';
+
+        if (!exception) {
+            return 'Expected function to throw an exception.';
+        }
+
+        if (!passed) {
+            return 'Expected function ' + not + 'to throw an exception which contains: "' + expectedMessages.join(', ') + '", but it threw "' + exceptionMessage + '".';
+        }
+    }
+
+    function executeAndReturnException(fn) {
+        try {
+            fn();
+        } catch (e) {
+            return e;
+        }
+    }
+
+    function exceptionContainsMessages(exceptionMessage, expected) {
+        return _.every(expected, function (expectedMessage) {
+            return exceptionMessage.indexOf(expectedMessage) >= 0;
+        });
+    }
+
+    function getExpectedMessages(expected) {
+        if (_.isString(expected)) {
+            return [expected];
+        }
+
+        if (_.isArray(expected)) {
+            return expected;
+        }
+
+        expected = jasmine.util.argsToArray(expected);
+
+        if (expected === undefined || expected.length === 0) {
+            throw new Error('Matcher "toThrowContaining" requires at least one string to be contained in the exception.');
+        }
+
+        return expected;
     }
 };
-
-function throwInvalidExpectationError() {
-    throw new Error('Matcher "toThrowContaining" requires at least one string to be contained in the exception.');
-};
-
-function getExceptionMessage(exception, exceptionMeetsExpectation, expected, exceptionMessage) {
-    var not = jasmine.isNot ? 'not ' : '';
-
-    if (!exception) {
-        return 'Expected function to throw an exception.';
-    }
-
-    if (!exceptionMeetsExpectation) {
-        return 'Expected function ' + not + 'to throw an exception which contains: "' + expected.join(', ') + '", but it threw "' + exceptionMessage + '".';
-    }    
-};
-
-
-function executeAndReturnException(fn) {
-    try {
-        fn();
-    } catch (e) {
-        return e;
-    }
-}
-
-function exceptionContainsMessages(exceptionMessage, expected) {
-    return _.every(expected, function (expectedMessage) {
-        return exceptionMessage.indexOf(expectedMessage) >= 0;
-    });
-}
--- src/matchers/to-throw-containing-matcher.spec.js
+++ src/matchers/to-throw-containing-matcher.spec.js
@@ -1,31 +1,54 @@
 describe('toThrowContainingMatcher', function () {
-    var toThrowContainingMatcher = require('./to-throw-containing-matcher'), test = {}, _ = require('lodash');
+    var toThrowContainingMatcher = require('./to-throw-containing-matcher'), _ = require('lodash');
 
     it('should throw an exception when actual is not a function', function () {
         expect(function () {
-            test.actual = 'foo';
+            var test = {actual: 'foo'};
             toThrowContainingMatcher().compare.call(null, test, 'foo');
         }).toThrow(new Error('The value is not a function in matcher "toThrowContaining".'));
     });
 
     it('should throw an exception when arguments is not an array or is empty', function () {
         expect(function () {
-            test.actual = _.noop;
+            var test = {actual: _.noop};
             toThrowContainingMatcher().compare.call(null, test, _.noop);
         }).toThrow(new Error('Matcher "toThrowContaining" requires at least one string to be contained in the exception.'));
     });
 
     it('should pass when the exception message contains the given messages', function () {
-        expect(function () {
-            throw new Error('Foo Bar');
-        }).toThrowContaining('Foo', 'Bar');
+        var expectedMessage, test = function () {
+                throw new Error(expectedMessage);
+            },
+            success = {
+                pass: true,
+                message: undefined
+            };
+
+        expectedMessage = 'Foo';
+        expect(toThrowContainingMatcher().compare.call(null, test, 'Foo')).toEqual(success);
+
+        expectedMessage = 'Foo Bar';
+        expect(toThrowContainingMatcher().compare.call(null, test, ['Foo', 'Bar'])).toEqual(success);
     });
 
-    it('should pass when negated and the exception not thrown or exception message does not contain the given messages', function () {
-        expect(_.noop).not.toThrowContaining('Bar');
-        expect(function () {
-            throw new Error('Foo');
-        }).not.toThrowContaining('Bar');
+    it('should not pass when the exception message does not contain all the given messages', function () {
+        var expectedMessage, test = function () {
+                throw new Error(expectedMessage);
+            },
+            fail = {
+                pass: false
+            };
+
+        expectedMessage = 'Foo';
+        fail.message = 'Expected function to throw an exception which contains: "Bar", but it threw "Foo".';
+        expect(toThrowContainingMatcher().compare.call(null, test, 'Bar')).toEqual(fail);
+
+        expectedMessage = 'Foo';
+        fail.message = 'Expected function to throw an exception which contains: "Foo Bar", but it threw "Foo".';
+        expect(toThrowContainingMatcher().compare.call(null, test, 'Foo Bar')).toEqual(fail);
     });
 
+    it('should pass when negated and no exception is thrown', function () {
+        expect(_.noop).not.toThrowContaining('Foo');
+    });
 });
